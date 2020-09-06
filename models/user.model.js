8b69c0b21b6d48bef242eb8b1d15c586abc91e6e
Pravat <pravatkiran.kumal@ehspl.com>
2020-09-06 19:29:40 +0530

    user authentication and customer api created

---
+++ models/user.model.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/connection');
+
+const User = sequelize.define('user', {
+    userid: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email: { type: Sequelize.STRING, autoNull : false},
+    password: { type: Sequelize.STRING, autoNull: false},
+    admin: Sequelize.BOOLEAN   
+});
+
+module.exports = User;
---
+++ routes/customer.route.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controllers/customer.controller');
+const Customer = require('../models/customer.model');
+const { check } = require('express-validator');
+
+router.post('/createCustomer', customerController.create);
+router.get('/getAll', customerController.getAll);
+router.get('/getById/:id', customerController.getById);
+router.put('/update/:id', check().custom((value, { req }) => {
+    return Customer.findByPk(req.params.id).then(customer => {
+        if (!customer) {
+            return Promise.reject('Customer not found')
+        }
+    })
+}), customerController.update);
+
+router.delete('/delete/:id', check().custom((value, { req }) => {
+    return Customer.findByPk(req.params.id).then(customer => {
+        if (!customer) {
+            return Promise.reject('Customer not found');
+        }
+    })
+}), customerController.delete);
+module.exports = router;
